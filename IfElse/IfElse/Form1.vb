f65a64c2f64afb2467ef1d2c5666edc0fe83249b
idsc3101 <idsc3101@gmail.com>
2013-09-23 15:21:07 -0500

    IfElse Problem Solved #2

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -5,8 +5,20 @@ Public Class Form1
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
 
         Dim num1, num2, largerNum As Double
+
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then
+            MessageBox.Show("Error: please type a number")
+            Return
+        End If
+
         num1 = CDbl(txtFirstNum.Text)
         num2 = CDbl(txtSecondNum.Text)
+
+        If num1 < 0 Or num1 < 0 Then
+            MessageBox.Show("Error: please type a positive number")
+            Return
+        End If
+
         If (num1) > num2 Then
             txtResult.Text = "Larger number is " & num1
         ElseIf (num2 > num1) Then
